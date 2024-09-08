e2bceb8e9bf0ca9a3149e0bf64ad184628601bcd
tamzishe <tamzsiwira@gmail.com>
2024-09-07 21:30:32 -0600

    update for render

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def numToWords(date):
     string = month + " " + date[8:10]+", " + date[0:4]
     return string
 
-if __name__== "__main__":
-    with app.app_context():
+with app.app_context():
         db.create_all()
+if __name__== "__main__":
     app.run(debug=True)
